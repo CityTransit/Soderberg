360f46f53e864f7062c146d6a2acb27bce18daf7
Robin Swanson <robinj.swanson@gmail.com>
2014-04-05 21:09:06 -0500

    First attempt at anisotropic kuwahara

--- src/frame.h
+++ src/frame.h
@@ -18,15 +18,24 @@ public:
 
     bool open(const char *fname);
     bool save(const char *filename, char *title);                           // Save image to file
+
     bool flip();
+
     bool applyKernel(Kernel *k);
     bool applyTwoToneKernel(Kernel *k);
     bool applyDoG(float s, float k);
+
     bool applyBilateral(float s, float r);
     bool applyTemporalBilateral(float s, float r, float t, Frame *f, int fn);
+
     bool applyKuwahara(int a);
+    bool applyAnisotropicKuwahara(int radius, float q);
+
     float threshold(float a, float b, float t);
     static float gaussian(float n, float sigma);
+
+    png_uint_32 getWidth() {return width;}
+    png_uint_32 getHeight() {return height;}
 private:
     static void userReadData(png_structp pngPtr, png_bytep data, png_size_t length);
     static bool validate(std::istream &src);
